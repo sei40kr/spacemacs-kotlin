3598a33f00a608218c76f1a194d4e99d0cd4a004
Seong Yong-ju <sei40kr@gmail.com>
2018-03-24 18:09:47 +0900

    Add packages.el
    Add empty file packages.el

---
+++ packages.el
@@ -0,0 +1,12 @@
+;;; packages.el - spacemacs-kotlin layer file for Spacemacs
+;;
+;; Copyright (c) 2018 Seong Yong-ju
+;;
+;; Author: Seong Yong-ju <sei40kr@gmail.com>
+;; URL: https://github.com/sei40kr/spacemacs-kotlin
+;;
+;; This file is not part of GNU Emacs
+;;
+;;; License: MIT
+
+(setq spacemacs-kotlin-packages '())
